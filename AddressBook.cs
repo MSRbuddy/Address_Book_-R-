87c871be4b3afd9501abbd7ff060807bb3d56d10
MSRbuddy <meghanadasari222@gmail.com>
2023-04-11 20:26:28 +0530

    Welcome to Address-Book prblm!

---
+++ AddressBook.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook
+{
+    internal class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to the Address Book Program!");
+        }
+    }
+}
